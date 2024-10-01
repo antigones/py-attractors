6fc258d516e0a01225c50de11ae4b799ba2d81e0
antigones <juna.salviati@gmail.com>
2024-10-01 20:16:44 +0200

    support drawing multiple solutions with their own color from a palette

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+import numpy as np
 from scipy.integrate import solve_ivp
 
 def lorenz_eq(t,state,S,R,B):
@@ -10,15 +12,31 @@ def lorenz_eq(t,state,S,R,B):
 
     return [dx_dt,dy_dt,dz_dt]
 
+def draw(solutions:list,colors:np.ndarray):
+    img = plt.figure().add_subplot(projection='3d')
+    for i,solution in enumerate(solutions):
+        img.plot(solution.y[0], solution.y[1], solution.y[2],color=colors[i])
+    plt.show()
+
+def animate(solutions:list,colors:np.ndarray):
+    pass
+
+cmap = mpl.colormaps['plasma']
 t_interval = (0,30)
 start_state = [1,1,1]
 
 S = 10
 R = 28
 B = 8/3
+solutions = list()
 
-sol = solve_ivp(lorenz_eq, t_interval, start_state, args=(S,R,B))
+N_SOL = 5
+for i in range(N_SOL):
+    sol = solve_ivp(lorenz_eq, t_interval, start_state, args=(S,R,B))
+    solutions.append(sol)
+    R = R + 0.0001
 
-img = plt.figure().add_subplot(projection='3d')
-img.plot(sol.y[0], sol.y[1], sol.y[2])
-plt.show()
+colors = cmap(np.linspace(0, 1, N_SOL))
+draw(solutions=solutions,colors=colors)
+
+
